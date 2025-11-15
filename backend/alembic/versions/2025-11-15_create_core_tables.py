42ef12ee8a32e25733b8780d419bebcbc604ce73
Dimas Abdullah Mufid <dimasmoveit@gmail.com>
2025-11-15 12:56:01 +0700

    fix: reorder import statements in core tables migration

--- backend/alembic/versions/2025-11-15_create_core_tables.py
+++ backend/alembic/versions/2025-11-15_create_core_tables.py
@@ -11,8 +11,9 @@ from __future__ import annotations
 from uuid import uuid4
 
 import sqlalchemy as sa
-from alembic import op
 from sqlalchemy.dialects import postgresql
+
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "2ee94a7d0a30"
